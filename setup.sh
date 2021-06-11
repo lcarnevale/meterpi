f18e343f53d4326544c79d156606f4e2b00ca3b2
Lorenzo Carnevale <lcarnevale@unime.it>
2021-06-11 07:42:35 +0100

    install libseccomp and docker as dependency

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
+sudo apt-get update && sudo apt-get upgrade
+
+echo("Installing Python dependencies ...")
 sudo apt install python3-pip python3-venv -y
+
+mkdir tmp
+cd tmp
+wget http://ftp.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.1-1_armhf.deb
+sudo dpkg -i libseccomp2_2.5.1-1_armhf.deb
+
+echo("Installing Docker dependency ...")
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker Pi
